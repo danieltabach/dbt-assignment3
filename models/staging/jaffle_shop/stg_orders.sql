6ba95b2232f98070ebd3cee206bd1726a8d509e5
daniel.tabach@baruchmail.cuny.edu <daniel.tabach@baruchmail.cuny.edu>
2021-11-21 06:02:21 +0000

    even more commitments.

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -4,8 +4,7 @@ with orders as (
         id as order_id,
         user_id as customer_id,
         order_date,
-        status as order_status
-
+        status
     from dbt-tutorial.jaffle_shop.orders
 )
 
